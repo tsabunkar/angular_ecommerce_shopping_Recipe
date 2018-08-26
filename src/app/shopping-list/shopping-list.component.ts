341635374c83e7ddf7d1e5ac572bce3de9e8dc0b
tsabunkar <tsabunkar@gmail.com>
2018-08-26 19:23:23 +0530

    Component is ready but without functionality

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredientsArray: Ingredient[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.ingredientsArray.push(this.ingredientObj1,this.ingredientObj2)
   }
+
+  ingredientObj1: Ingredient = new Ingredient('Apple', 5);
+  ingredientObj2: Ingredient = new Ingredient('Banana', 10);
 
 }
